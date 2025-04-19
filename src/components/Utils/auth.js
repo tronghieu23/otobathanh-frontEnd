6566561ae3a29688d96427650d03c6de2bd3c509
HoanggSangg <taolasang2k1@gmail.com>
2025-04-19 16:23:10 +0700

    fix token error for login

--- src/components/Utils/auth.js
+++ src/components/Utils/auth.js
@@ -1,7 +1,30 @@
+import { jwtDecode } from 'jwt-decode';
+
 export const getCurrentUser = () => {
     try {
         const userStr = localStorage.getItem('user');
-        return userStr ? JSON.parse(userStr) : null;
+        const token = localStorage.getItem('token');
+
+        // Nếu không có token hoặc user thì trả về null
+        if (!userStr || !token) {
+            return null;
+        }
+
+        // Giải mã token
+        const decodedToken = jwtDecode(token);
+
+        const currentTime = Date.now() / 1000; // Thời gian hiện tại tính bằng giây
+
+        // Kiểm tra xem token đã hết hạn chưa
+        if (decodedToken.exp < currentTime) {
+            // Nếu hết hạn, xóa token và user khỏi localStorage
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            return null; // Token hết hạn, không trả về user
+        }
+
+        // Nếu chưa hết hạn, trả về user
+        return JSON.parse(userStr);
     } catch (err) {
         console.error('Lỗi đọc user:', err);
         return null;
